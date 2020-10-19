80720ac1263e93c69ceb6d72f49a528e68d7f377
ADarien <aalian.darien@gmail.com>
2020-10-19 16:20:49 +0700

    just save

--- draft.go
+++ draft.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"unicode"
-)
+import "fmt"
 
 func main() {
-	var text string
-	fmt.Scan(&text)
-	rs := []rune(text)
-	var isLatin, isOkLetter, isLong bool
-	for i := range rs {
-		if unicode.Is(unicode.Latin, rs[i]) || unicode.IsLetter(rs[i]) || unicode.IsDigit(rs[i]) {
-			isOkLetter = true
-		} else {
-			isOkLetter = false
-			break
-		}
-		if unicode.Is(unicode.Latin, rs[i]) || unicode.IsDigit(rs[i]) {
-			isLatin = true
-		} else {
-			isLatin = false
-			break
-		}
-
-	}
-	if len(rs) >= 5 {
-		isLong = true
-	}
-
-	if isLatin && isOkLetter && isLong {
-		fmt.Println("Ok")
-	} else {
-		fmt.Println("Wrong password")
-	}
-
-	fmt.Println(isLatin, isOkLetter, isLong)
+	fmt.Println("Hola!")
 }

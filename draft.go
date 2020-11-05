7ed2cdb50cfc274a6b30c8e9f524b41f4b76252f
ADarien <aalian.darien@gmail.com>
2020-11-05 17:21:35 +0700

    just save

--- draft.go
+++ draft.go
@@ -1,32 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
-	"unicode"
+	"strings"
 )
 
-func adding(s1, s2 string) int64 {
-	string1 := ""
-	for _, ch := range s1 {
-		if unicode.IsDigit(ch) {
-			string1 += string(ch)
-		}
-	}
-
-	string2 := ""
-	for _, ch := range s2 {
-		if unicode.IsDigit(ch) {
-			string2 += string(ch)
-		}
-	}
-	ch1, _ := strconv.ParseInt(string1, 10, 64)
-	ch2, _ := strconv.ParseInt(string2, 10, 64)
-	res := ch1 + ch2
-	// fmt.Println(res)
-
-	return res
-}
-
 func main() {
-	adding("%^80", "hhhhh20&&&&nd")
+	// str, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	// str = strings.Trim(str, "\n")
+
+	str := "1 149,6088607594936;1 179,0666666666666"
+	lineName := strings.Split(string(str), ";")
+
+	ch1 := strings.Replace(lineName[0], " ", "", 1)
+	ch2 := strings.Replace(lineName[1], " ", "", 1)
+
+	ch1 = strings.Replace(ch1, ",", ".", 1)
+	ch2 = strings.Replace(ch2, ",", ".", 1)
+
+	fmt.Println(ch1)
+	fmt.Println(ch2)
+
+	digit1, _ := strconv.ParseFloat(ch1, 64)
+	digit2, _ := strconv.ParseFloat(ch2, 64)
+
+	// fmt.Println(digit1 / digit2)
+	res := digit1 / digit2
+
+	text := strconv.FormatFloat(res, 'f', 4, 64)
+	rs := []rune(text)
+
+	fmt.Println(rs)
+
 }

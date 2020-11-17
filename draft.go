17f322f23f72deb63d10e3b7b4c18bcf86e08e5d
ADarien <aalian.darien@gmail.com>
2020-11-17 15:58:03 +0700

    read csv

--- draft.go
+++ draft.go
@@ -1,44 +1,25 @@
 package main
 
 import (
-	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
+	"path/filepath"
 )
 
-// Swap func
-func Swap(x []int, y int) {
-	var a int
-	a = x[y]
-	x[y] = x[y+1]
-	x[y+1] = a
-}
-
-// BubbleSort func
-func BubbleSort(sl []int) {
-	for i := range sl {
-		for j := 0; j < len(sl)-i-1; j++ {
-			if sl[j] > sl[j+1] {
-				Swap(sl, j)
+func main() {
+	filepath.Walk("./task", func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			file, _ := os.Open(path)
+			cr := csv.NewReader(file)
+			rowIndex := 0
+			for row, err := cr.Read(); err == nil; row, err = cr.Read() {
+				if rowIndex == 4 {
+					fmt.Println(row[2])
+				}
+				rowIndex++
 			}
 		}
-	}
-}
-
-func main() {
-	fmt.Println("Input 10 numbers:")
-	numbers := bufio.NewReader(os.Stdin)
-	readStr, _, _ := numbers.ReadLine()
-	nums := strings.Split(string(readStr), " ")
-
-	var numSlice []int
-	for _, num := range nums {
-		i, _ := strconv.Atoi(num)
-		numSlice = append(numSlice, i)
-	}
-	BubbleSort(numSlice)
-	fmt.Println(numSlice)
-
+		return nil
+	})
 }

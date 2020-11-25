4b80f380109fd43c064f0270ce56ac4451c0d9fc
ADarien <aalian.darien@gmail.com>
2020-11-25 14:26:33 +0700

    goroutine

--- draft.go
+++ draft.go
@@ -1,36 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
+
+func prod(v1 int, v2 int, c chan int) {
+	c <- v1 * v2
+}
 
 func main() {
-	// str, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	// str = strings.Trim(str, "\n")
-
-	str := "1 149,6088607594936;1 179,0666666666666"
-	lineName := strings.Split(string(str), ";")
-
-	ch1 := strings.Replace(lineName[0], " ", "", 1)
-	ch2 := strings.Replace(lineName[1], " ", "", 1)
-
-	ch1 = strings.Replace(ch1, ",", ".", 1)
-	ch2 = strings.Replace(ch2, ",", ".", 1)
-
-	fmt.Println(ch1)
-	fmt.Println(ch2)
-
-	digit1, _ := strconv.ParseFloat(ch1, 64)
-	digit2, _ := strconv.ParseFloat(ch2, 64)
-
-	// fmt.Println(digit1 / digit2)
-	res := digit1 / digit2
-
-	text := strconv.FormatFloat(res, 'f', 4, 64)
-	rs := []rune(text)
-
-	fmt.Println(rs)
-
+	c := make(chan int)
+	go prod(1, 2, c)
+	go prod(3, 4, c)
+	a := <-c
+	b := <-c
+	fmt.Println(a * b)
 }
